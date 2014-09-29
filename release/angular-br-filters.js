d152b0b752240b0233e76887fd02d5c55f4b7d5f
Igor Rafael <igor.rafael@gmail.com>
2014-09-29 17:43:45 -0300

    Build configuration

---
+++ release/angular-br-filters.js
@@ -0,0 +1,173 @@
+/**
+ * angular-br-filters
+ * angular-br-filters ==================
+ * @version v0.1.0
+ * @link https://github.com/the-darc/angular-br-filters
+ * @license MIT
+ */
+(function (angular) {
+	var global = {};
+
+/**
+ * br-masks
+ * A library of masks applicable to several Brazilian data like I.E., CNPJ, CPF and others
+ * @version v0.1.0
+ * @link http://github.com/the-darc/br-masks
+ * @license MIT
+ */
+(function () {
+  var root = this;
+if (typeof require === 'function') {
+	var StringMask = require('string-mask');
+}
+
+var CEP = function(value) {
+	var cepMask = new StringMask('00000-000');
+	if(!value) {
+		return value;
+	}
+	var processed = cepMask.process(value);
+	return processed.result;
+};
+
+if (typeof require === 'function') {
+	var StringMask = require('string-mask');
+}
+
+var CNPJ = function(value) {
+	if(!value) {
+		return value;
+	}
+	var cnpjPattern = new StringMask('00.000.000\/0000-00');
+	var formatedValue = cnpjPattern.apply(value);
+	return formatedValue;
+};
+
+if (typeof require === 'function') {
+	var StringMask = require('string-mask');
+}
+
+var CPF = function(value) {
+	var cpfPattern = new StringMask('000.000.000-00');
+	if(!value) {
+		return value;
+	}
+	var formatedValue = cpfPattern.apply(value);
+	return formatedValue;
+};
+
+if (typeof require === 'function') {
+	var StringMask = require('string-mask');
+}
+
+var IE = function(value, uf) {
+	var ieMasks = {
+		'AC': [{mask: new StringMask('00.000.000/000-00')}],
+		'AL': [{mask: new StringMask('000000000')}],
+		'AM': [{mask: new StringMask('00.000.000-0')}],
+		'AP': [{mask: new StringMask('000000000')}],
+		'BA': [{chars: 8, mask: new StringMask('000000-00')},
+			   {mask: new StringMask('0000000-00')}],
+		'CE': [{mask: new StringMask('00000000-0')}],
+		'DF': [{mask: new StringMask('00000000000-00')}],
+		'ES': [{mask: new StringMask('00000000-0')}],
+		'GO': [{mask: new StringMask('00.000.000-0')}],
+		'MA': [{mask: new StringMask('000000000')}],
+		'MG': [{mask: new StringMask('000.000.000/0000')}],
+		'MS': [{mask: new StringMask('000000000')}],
+		'MT': [{mask: new StringMask('0000000000-0')}],
+		'PA': [{mask: new StringMask('00-000000-0')}],
+		'PB': [{mask: new StringMask('00000000-0')}],
+		'PE': [{chars: 9, mask: new StringMask('0000000-00')},
+			   {mask: new StringMask('00.0.000.0000000-0')}],
+		'PI': [{mask: new StringMask('000000000')}],
+		'PR': [{mask: new StringMask('000.00000-00')}],
+		'RJ': [{mask: new StringMask('00.000.00-0')}],
+		'RN': [{chars: 9, mask: new StringMask('00.000.000-0')},
+			   {mask: new StringMask('00.0.000.000-0')}],
+		'RO': [{mask: new StringMask('0000000000000-0')}],
+		'RR': [{mask: new StringMask('00000000-0')}],
+		'RS': [{mask: new StringMask('000/0000000')}],
+		'SC': [{mask: new StringMask('000.000.000')}],
+		'SE': [{mask: new StringMask('00000000-0')}],
+		'SP': [{mask: new StringMask('000.000.000.000')},
+			   {mask: new StringMask('-00000000.0/000')}],
+		'TO': [{mask: new StringMask('00000000000')}]
+	};
+
+	function clearValue (value) {
+		if(!value) {
+			return value;
+		}
+
+		return value.replace(/[^0-9]/g, '');
+	}
+
+	function getMask(uf, value) {
+		if(!uf || !ieMasks[uf]) {
+			return undefined;
+		}
+		var _uf = uf.toUpperCase();
+		if (_uf === 'SP' && /^P/i.test(value)) {
+			return ieMasks.SP[1].mask;
+		}
+		var masks = ieMasks[uf];
+		var i = 0;
+		while(masks[i].chars && masks[i].chars < clearValue(value).length && i < masks.length - 1) {
+			i++;
+		}
+		return masks[i].mask;
+	}
+
+	var mask = getMask(uf, value);
+	if(!value || !mask) {
+		return value;
+	}
+	var processed = mask.process(clearValue(value));
+	if (uf && uf.toUpperCase() === 'SP' && /^p/i.test(value)) {
+		return 'P'+processed.result;
+	}
+	return processed.result;
+};
+
+if (typeof require === 'function') {
+	var StringMask = require('string-mask');
+}
+
+var PHONE = function(value) {
+	var phoneMask8D = new StringMask('(00) 0000-0000'),
+		phoneMask9D = new StringMask('(00) 00000-0000');
+
+	if(!value) {
+		return value;
+	}
+
+	var formatedValue;
+	if(value.length < 11){
+		formatedValue = phoneMask8D.apply(value);
+	}else{
+		formatedValue = phoneMask9D.apply(value);
+	}
+
+	return formatedValue;
+};
+
+var BrM = {
+   ie: IE,
+   cpf: CPF,
+   cnpj: CNPJ,
+   phone: PHONE,
+   cep: CEP
+};
+var objectTypes = {
+	'function': true,
+	'object': true
+};
+if (objectTypes[typeof module]) {
+	module.exports = BrM;
+} else {
+	root.BrM = BrM;
+}
+}.call(this));
+
+})(angular);
