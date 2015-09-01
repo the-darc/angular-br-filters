ba28b54c9cca9ba1c919100e72af4388b2105e01
darc <darc.tec@gmail.com>
2015-09-01 10:46:02 -0300

    chore: version 0.5.0

--- release/angular-br-filters.js
+++ release/angular-br-filters.js
@@ -1,7 +1,7 @@
 /**
  * angular-br-filters
  * An Angular library of masks applicable to several Brazilian data.
- * @version v0.4.0
+ * @version v0.5.0
  * @link https://github.com/the-darc/angular-br-filters
  * @license MIT
  */(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
